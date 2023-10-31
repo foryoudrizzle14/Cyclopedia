28aabf726c44c2051407b40f54ee57b0ff06c0f5
foryoudrizzle141~ <foryoudrizzle14@gmail.com>
2023-10-31 18:41:14 +0900

    LifeCyclopedia update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import CyclOPediaClassPage from "./CyclOpediaClassPage";
+import CyclOPediaFuncPage from "./CyclOpediaFuncPage";
 import Header from "./Layout/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header></Header>);
+root.render(
+  <div>
+    <Header />
+    <div className="row text-white">
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Class Component</span>
+        <CyclOPediaClassPage />
+      </div>
+      <div className="col-6">
+        <span className="h1 text-warning text-center">Func Component</span>
+        <CyclOPediaFuncPage />
+      </div>
+    </div>
+  </div>
+);
